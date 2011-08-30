848bc9672662a0f0b508f36752a0ac1ff7847a86
Naoyuki Kumagai <kumagai@mimas>
2011-08-30 17:00:15 +0900

    Add before_filter :authenticate to ApplicationController
    app/controllers/application_controller.rb
    - Add before_filter :authenticate.
    public/stylesheets/user_auth_kuma.css
    - Plugin user_auth corrected for notice and alert.
    vendor/plugins/user_auth
    - Subproject updated to 640d264efb187a18cec56c939f30f32b6d18817f.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+
   protect_from_forgery :secret => '7d35786ab90a0561c3ca228af02809338b47c77c'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110830064022) do
+ActiveRecord::Schema.define(:version => 20110830064941) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -21,5 +21,14 @@ ActiveRecord::Schema.define(:version => 20110830064022) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string   "name",                                :null => false
+    t.string   "hashed_password"
+    t.boolean  "is_writer",        :default => false, :null => false
+    t.boolean  "is_administrator", :default => false, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
