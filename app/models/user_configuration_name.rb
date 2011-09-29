88a08c6264076e920662c50ac22a0d9a7343848a
Naoyuki Kumagai <kumagai@mimas>
2011-09-29 17:02:56 +0900

    Add validates for uniqueness to model UserConfiguration{Name,Value}
    app/models/user.rb
    - Add get_conf_value().
    app/models/user_configuration_name.rb
    - Add validates :name, :uniqueness => true.
    app/models/user_configuration_value.rb
    - Add validates :user_configuration_name_id, :uniqueness => {:scope => :user_id}.
    vendor/plugins/user_conf
    - Subproject updated to 41db6cf56c82c7a9908ecae172e8bbc914dfc573.

--- app/models/user_configuration_name.rb
+++ app/models/user_configuration_name.rb
@@ -1,3 +1,5 @@
 class UserConfigurationName < ActiveRecord::Base
   has_many :user_configuration_values
+
+  validates :name, :uniqueness => true
 end
--- app/models/user_configuration_value.rb
+++ app/models/user_configuration_value.rb
@@ -1,4 +1,6 @@
 class UserConfigurationValue < ActiveRecord::Base
   belongs_to :user
   belongs_to :user_configuration_name
+
+  validates :user_configuration_name_id, :uniqueness => {:scope => :user_id}
 end
